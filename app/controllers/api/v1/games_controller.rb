6d5f620069097e1faeabad365df74cb43641ad69
Seth Schwartz <sschwartz214@gmail.com>
2018-09-12 11:48:53 -0600

    working on games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::GamesController < ApplicationController
   def show
-    render json: {}
+    # p1 = User.find(params[:id])
+    # p2 = User.find(params[:id])
+    # game = Game.find(params[:player_1_id], params[:player_2_id])
+    # Play.find(params[:id])
+    # Play.find(params[:id])
+    game = Game.find_by(id: params[:id])
+
+    render json: game
   end
 end
--- spec/requests/game_spec.rb
+++ spec/requests/game_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe 'Game API' do
   it 'sends scores' do
-    p1 = User.create(name: "Seth")
-    p2 = User.create(name: "Joe")
-    game = Game.create(player_1_id: p1.id, player_2_id: p2.id)
-    p1_score = p1.plays.create(game_id: 1, word: "hi", score: 15)
-    p2_score = p2.plays.create(game_id: 1, word: "bye", score: 16)
-    
+    josh = User.create(id: 1, name: "Josh")
+    sal = User.create(id: 2, name: "Sal")
+
+    game = Game.create(player_1: josh, player_2: sal)
+
+    josh.plays.create(game: game, word: "sal", score: 15)
+    sal.plays.create(game: game, word: "josh", score: 16)
+
     get "/api/v1/games/1"
 
     expect(response).to be_successful
-    require "pry"; binding.pry
+
     score = JSON.parse(response.body, symbolize_names: true)
 
     return_json =  {
