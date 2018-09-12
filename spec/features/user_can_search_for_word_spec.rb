f06605601b1ac6e553beafc4c07635249d335289
Seth Schwartz <sschwartz214@gmail.com>
2018-09-12 09:26:46 -0600

    first commit

---
+++ spec/features/user_can_search_for_word_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "As a guest user" do
+  context "visiting root page" do
+    it "searches for a word" do
+# As a guest user (no sign in necessary)
+# When I visit "/"
+visit '/'
+# And I fill in a text box with "foxes"
+fill_in :q, with: "foxes"
+# And I click "Validate Word"
+click_on "Validate Word"
+# Then I should see a message that says "'foxes' is a valid word and its root form is 'fox'."
+expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'.")
+    end
+  end
+end
+
+# As a guest user
+# When I visit "/"
+# And I fill in a text box with "foxez"
+# And I click "Validate Word"
+# Then I should see a message that says "'foxez' is not a valid word."
