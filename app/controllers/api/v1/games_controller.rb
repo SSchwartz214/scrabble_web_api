d95fdfddf540fe32c159f657f9caaefad9dba21a
Seth Schwartz <sschwartz214@gmail.com>
2018-09-12 11:33:03 -0600

    creating test for US 2

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::GamesController < ApplicationController
+  def show
+    render json: {}
+  end
 end
--- spec/requests/game_spec.rb
+++ spec/requests/game_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe 'Game API' do
   it 'sends scores' do
+    p1 = User.create(name: "Seth")
+    p2 = User.create(name: "Joe")
+    game = Game.create(player_1_id: p1.id, player_2_id: p2.id)
+    p1_score = p1.plays.create(game_id: 1, word: "hi", score: 15)
+    p2_score = p2.plays.create(game_id: 1, word: "bye", score: 16)
     
-
     get "/api/v1/games/1"
 
     expect(response).to be_successful
+    require "pry"; binding.pry
     score = JSON.parse(response.body, symbolize_names: true)
 
     return_json =  {
