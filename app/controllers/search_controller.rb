45c2a3da456d8e3f92d0a79d6b202cd2a12ecee3
Seth Schwartz <sschwartz214@gmail.com>
2018-09-12 22:35:57 -0600

    creates sad path for invalid route

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,11 @@ class SearchController < ApplicationController
 
   def index
     @presenter = SearchPresenter.new(params[:q])
+    @word = params[:q]
+    if @presenter.service.status == 200
+      render :index
+    elsif @presenter.service.status == 404
+      render :invalid
+    end
   end
-
 end
--- app/presenters/search_presenter.rb
+++ app/presenters/search_presenter.rb
@@ -5,18 +5,7 @@ class SearchPresenter
   end
 
   def word
-    # @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
-    #   faraday.headers["app_id"] = "6f7c243e"
-    #   faraday.headers["app_key"] = "32559a1c0fcb2bcdd88ae8627531abda"
-    #   faraday.adapter  Faraday.default_adapter
-    # end
-    # response = @conn.get("/api/v1/inflections/en/#{@word}")
-    # if response.status == 404
-    #   "'#{@word}' is not a valid word."
-    #
-    # else
-    #   results = JSON.parse(response.body, symbolize_names: true)[:results][0]
-      Word.new(service.book_results)
+    Word.new(service.book_results)
   end
 
   def service
